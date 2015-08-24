6c3005ac4ef55192fd6d97ff3dd98b02849aa447
Josselyn Medina <josselynmedina@unitec.edu>
2015-08-23 20:01:22 -0600

    Nuevo

---
+++ ASKI-VACACIONES/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASKI_VACACIONES.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Login(){return View();}
+        public ViewResult MenuPrincipal() { return View(); }
+        public ViewResult Calendario() { return View(); }
+        public ViewResult Administracion() { return View(); }
+        public ViewResult NuevaSolicitud() { return View(); }
+        public ViewResult MisSolicitudes() { return View(); }
+    }
+}
