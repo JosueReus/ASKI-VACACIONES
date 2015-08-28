629e4ca2eb29c7c0ffd99d6f9f27b962fb537ff5
Josselyn Medina <josselynmedina@unitec.edu>
2015-08-28 01:16:18 -0600

    cambios

--- ASKI-VACACIONES/Models/UsuariosModel.cs
+++ ASKI-VACACIONES/Models/UsuariosModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,14 +9,29 @@ namespace ASKI_VACACIONES.Models
     public class UsuariosModel
     {
         public int talento_humano { get; set; }
+
+        [Required]
+       
+       
         public string email { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string primer_nombre { get; set; }
+         [Required]
         public string segundo_nombre { get; set; }
+         [Required]
         public string primer_apellido { get; set; }
+         [Required]
         public string segundo_apellido { get; set; }
+         [Required]
         public DateTime fecha_ingreso { get; set; }
+         [Required]
         public DateTime fecha_creacion { get; set; }
+         [Required]
         public string password { get; set; }
+         [Required]
         public bool activo { get; set; }
+         [Required]
     }
 }
