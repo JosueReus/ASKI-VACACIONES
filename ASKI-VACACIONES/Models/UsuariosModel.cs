966edbbc64d109108664f96136abfcd8026cfa6e
Josselyn Medina <josselynmedina@unitec.edu>
2015-08-28 23:09:09 -0600

    validacion de los id

--- ASKI-VACACIONES/Models/UsuariosModel.cs
+++ ASKI-VACACIONES/Models/UsuariosModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -8,29 +9,27 @@ namespace ASKI_VACACIONES.Models
 {
     public class UsuariosModel
     {
+        [Required]
         public int talento_humano { get; set; }
 
         [Required]
-       
-       
         public string email { get; set; }
 
-        [Required]
-        [EmailAddress]
+        
         public string primer_nombre { get; set; }
-         [Required]
+         
         public string segundo_nombre { get; set; }
-         [Required]
+      
         public string primer_apellido { get; set; }
-         [Required]
+        
         public string segundo_apellido { get; set; }
-         [Required]
+        
         public DateTime fecha_ingreso { get; set; }
-         [Required]
+       
         public DateTime fecha_creacion { get; set; }
-         [Required]
+         
         public string password { get; set; }
-         [Required]
+        
         public bool activo { get; set; }
       
     }
